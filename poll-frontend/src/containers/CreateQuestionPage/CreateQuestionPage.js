19165948b2edfb7c669e9fd73249e5f7dd31b542
Amit Dimri <dimriamit98@gmail.com>
2019-08-30 22:55:12 +0530

    create question

--- poll-frontend/src/containers/CreateQuestionPage/CreateQuestionPage.js
+++ poll-frontend/src/containers/CreateQuestionPage/CreateQuestionPage.js
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
+import axios from "axios";
+
+// config
+import config from "../../config";
+import { getCookie } from "../../helper";
 
 import Textarea from "../../components/Textarea";
+import Button from "../../components/Button/Button";
+import Mcq from "./Mcq";
 
 // CSS
 import "./CreateQuestion.scss";
-import Button from "../../components/Button/Button";
-
 const CreateQuestionPage = params => {
   const [questionData, setQuestionData] = useState({
     question_text: "",
-    poll_image: "",
+    poll_image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRnlq_ULfQCgyOG7-2umjBD5ZrCvj3nl09oanM3Ue-lMrMfLZo5",
     poll_type: 1
   });
   const [anonymous, setAnonymous] = useState(false);
   const [isPublic, setIsPublic] = useState(true);
+
+  const [choices, setChoices] = useState([]);
+  const [choice, setChoice] = useState("");
 
   const handleQueChange = ({ target }) => {
     let { name, value } = target;
@@ -36,10 +45,39 @@ const CreateQuestionPage = params => {
     }
   };
 
+  const sendChoices = async choice => {
+    console.log("choice::::", choice);
+
+    let { data } = await axios.post(`${config.API_URL}/choice/`, choice);
+
+    console.log("response choice::::", data);
+  };
+
+  const sendQuestionData = async questionData => {
+    console.log("send question data:::", questionData);
+
+    let { data } = await axios.post(`${config.API_URL}/polls/`, questionData);
+
+    console.log("response questinData::::", data);
+
+    choices.forEach(v => {
+      sendChoices({ poll: data.id, choice_text: v.value });
+    });
+  };
+
   const handleSubmit = e => {
     e.preventDefault();
 
-    console.log("dataL:::", { ...questionData, anonymous, isPublic });
+    let _questionData = {
+      ...questionData,
+      anonymous_user: anonymous,
+      public: isPublic,
+      user: JSON.parse(getCookie("auth")).id
+    };
+
+    sendQuestionData(_questionData);
+
+    window.location.href = "/";
   };
 
   return (
@@ -55,9 +93,6 @@ const CreateQuestionPage = params => {
         <div>
           <label htmlFor="">Poll Type</label>
           <select name="poll_type" defaultValue="1" onChange={handleQueChange}>
-            {/* <option value="" defaultChecked disabled>
-              Choose Poll Type
-            </option> */}
             <option value="0">MCQ</option>
             <option value="1" defaultChecked>
               Text Based
@@ -65,7 +100,16 @@ const CreateQuestionPage = params => {
           </select>
         </div>
 
-        {questionData.poll_type === 0 ? <p>MCQ</p> : ""}
+        {questionData.poll_type === 0 ? (
+          <Mcq
+            choice={choice}
+            choices={choices}
+            setChoice={setChoice}
+            setChoices={setChoices}
+          />
+        ) : (
+          ""
+        )}
 
         <div>
           <input
